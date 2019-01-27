4bb1cd92e08fa4bed137b08909296402b3625d48
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 20:08:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,8 @@ function removeElementFromBeginningOfArray(array) {
     new_array = array.slice(1)
     return new_array
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+    array.pop()
+    return array
+}
