fa91fd9750dbb515bddb895a098c3ea1408d5b26
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 20:05:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-    array.unshift(element)
+    array.push(element)
     return array
 }
 
