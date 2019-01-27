fedc5dcd52fe9f623447a419c6f8baec93de13d9
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 19:38:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
     return array.unshift(element)
 }
+
+function addElementToEndOfArray(array, element) {
+    return array.shift(element)
+}
